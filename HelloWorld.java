9c2a7a224d567a126083704206aedf12d8550837
thienmanhvo <benficacronaldo@gmail.com>
2018-11-15 00:34:37 +0700

    First java program

---
+++ HelloWorld.java
@@ -0,0 +1,26 @@
+
+import java.util.ArrayList;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author benfi
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+//        ArrayList arr = new ArrayList();
+//        arr.add(1);
+//        for (int i = 0; i < 2; i++) {
+//            System.out.println(arr.get(i));
+//        }
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Dit cu may Mai Linh");
+        }
+    }
+}
